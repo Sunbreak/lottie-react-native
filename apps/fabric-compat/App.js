de7dada111295febfc2585346e8b91bc31753556
wangkun <wangkun@xiaohongshu.com>
2023-07-21 15:39:05 +0800

    chore: add lottie-react-native to fabric-compat

--- apps/fabric-compat/App.js
+++ apps/fabric-compat/App.js
@@ -1,112 +1,122 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import LottieView from 'lottie-react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const color = {
+  primary: '#1652f0',
+  secondary: '#64E9FF',
+};
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const remoteSource = {
+  uri: 'https://raw.githubusercontent.com/lottie-react-native/lottie-react-native/master/apps/paper/src/animations/Watermelon.json',
+};
+
+const localSource = require('./animations/LottieLogo1.json');
+
+const App: () => Node = () => {
+  const [source, setSource] = React.useState('remote');
+  const [isLoop, setLoop] = React.useState(false);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <LottieView
+        key={source + isLoop}
+        source={source === 'remote' ? remoteSource : localSource}
+        autoPlay={true}
+        loop={isLoop}
+        style={styles.lottie}
+        resizeMode={'contain'}
+        colorFilters={colorFilter}
+        enableMergePathsAndroidForKitKatAndAbove
+        onAnimationFinish={() => {
+          console.log('finished');
+        }}
+      />
+      <View style={styles.controlsContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            setSource(source === 'local' ? 'remote' : 'local');
+          }}
+          style={styles.button}>
+          <Text style={styles.text}>
+            {source === 'local' ? 'Local animation' : 'Remote animation'}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setLoop(p => !p);
+          }}
+          style={styles.button}>
+          <Text style={styles.text}>{isLoop ? 'Loop on' : 'Loop off'}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 32,
+  },
+  controlsContainer: {flexDirection: 'row', marginTop: 24, columnGap: 12},
+  button: {
+    backgroundColor: color.primary,
     paddingHorizontal: 24,
+    paddingVertical: 16,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  text: {color: 'white'},
+  lottie: {width: 400, height: 400},
 });
+
+const colorFilter = [
+  {
+    keypath: 'BG',
+    color: color.primary,
+  },
+  {
+    keypath: 'O-B',
+    color: color.secondary,
+  },
+  {
+    keypath: 'L-B',
+    color: color.secondary,
+  },
+  {
+    keypath: 'T1a-Y 2',
+    color: color.secondary,
+  },
+  {
+    keypath: 'T1b-Y',
+    color: color.secondary,
+  },
+  {
+    keypath: 'T2b-B',
+    color: color.secondary,
+  },
+  {
+    keypath: 'T2a-B',
+    color: color.secondary,
+  },
+  {
+    keypath: 'I-Y',
+    color: color.secondary,
+  },
+  {
+    keypath: 'E1-Y',
+    color: color.secondary,
+  },
+  {
+    keypath: 'E2-Y',
+    color: color.secondary,
+  },
+  {
+    keypath: 'E3-Y',
+    color: color.secondary,
+  },
+];
 
 export default App;
